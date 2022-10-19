3aa6e5cf08b67357febba39178327210bd94e747
Guarmouch.khaoula <111585262+guarmouch@users.noreply.github.com>
2022-10-19 13:19:07 +0100

    add film and list of movies are functioning

--- src/main/java/com/hendisantika/repository/SeanceRepository.java
+++ src/main/java/com/hendisantika/repository/SeanceRepository.java
@@ -1,5 +1,7 @@
 package com.hendisantika.repository;
 
+import java.util.Date;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,12 +9,11 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.hendisantika.entity.Nationalite;
 import com.hendisantika.entity.Salle;
+import com.hendisantika.entity.Seance;
 
 @CrossOrigin("http://localhost:4200")
 @Repository
-
-public interface SalleRepository extends JpaRepository<Salle, Long> {
-	Page<Salle> findByNumeroStartsWith(@RequestParam("numero")  int numero, Pageable pageable);
+public interface SeanceRepository extends JpaRepository<Seance, Long> {
+	Page<Seance> findByDateProjection(@RequestParam("dateProjection")  Date dateProjection, Pageable pageable);
 }
