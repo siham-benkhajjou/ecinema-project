483d6b268f267d38ec484b9929a5c55741af62b0
siham benkhajjou <114482261+siham-benkhajjou@users.noreply.github.com>
2022-10-20 14:42:57 +0100

    fix typepersone

--- src/main/java/com/hendisantika/entity/Salle.java
+++ src/main/java/com/hendisantika/entity/Salle.java
@@ -6,15 +6,11 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Getter
--- src/main/java/com/hendisantika/entity/Seance.java
+++ src/main/java/com/hendisantika/entity/Seance.java
@@ -7,13 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Getter
